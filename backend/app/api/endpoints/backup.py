22bee5d80f78a01511294d6c7aaa3b1e830cf4d8
shivam sharma <sharmas6147@gmail.com>
2026-04-29 12:53:50 +0530

    Uncomment MYSQLPORT in backup.py

--- backend/app/api/endpoints/backup.py
+++ backend/app/api/endpoints/backup.py
@@ -16,7 +16,7 @@ def generate_sql_dump():
         user=os.getenv("MYSQLUSER"),
         password=os.getenv("MYSQLPASSWORD"),
         database=os.getenv("MYSQLDATABASE"),
-        # port=int(os.getenv("MYSQLPORT", 3306)),
+        port=int(os.getenv("MYSQLPORT", 3306)),
         cursorclass=pymysql.cursors.Cursor
     )
 
