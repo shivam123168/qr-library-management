49ce1dfe1b6329063fe59bd81bf65312f0887420
shivam sharma <sharmas6147@gmail.com>
2026-04-29 12:50:31 +0530

    Update database connection environment variables

--- backend/app/api/endpoints/backup.py
+++ backend/app/api/endpoints/backup.py
@@ -12,10 +12,11 @@ router = APIRouter()
 
 def generate_sql_dump():
     conn = pymysql.connect(
-        host=os.getenv("mysql.railway.internal"),
-        user=os.getenv("root"),
-        password=os.getenv("yMRswONyrTtWljNzfwSjozVXnQCqOweM"),
-        database=os.getenv("railway"),
+        host=os.getenv("MYSQLHOST"),
+        user=os.getenv("MYSQLUSER"),
+        password=os.getenv("MYSQLPASSWORD"),
+        database=os.getenv("MYSQLDATABASE"),
+        # port=int(os.getenv("MYSQLPORT", 3306)),
         cursorclass=pymysql.cursors.Cursor
     )
 
